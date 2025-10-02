805f899bce5fe79654df3d133ab32265335a0213
Chaliptul <apinutchaya.w@ku.th>
2025-10-02 17:25:07 +0700

    Apple.c

--- Apple.c
+++ Apple.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 int main(void) {
-    const double BEAKER_CAPACITY = 5000.0; //beaker
-    const double POUR = 250.0; //pour per one
-    double weight_g; //apple
-    double water_volume = 0.0; //volume
+    const double a = 5000.0; //beaker
+    const double b = 250.0; //pour per one
+    double c; //apple
+    double d = 0.0; //volume
     int pours = 0; //time the pour
     printf("Enter the apple weight (g) :  ");
-    if (scanf("%lf", &weight_g) != 1) {
+    if (scanf("%lf", &c) != 1) {
         printf("Input wrong \n");
         return 1;
     }
 
-    if (weight_g <= 0) {
+    if (c <= 0) {
         printf("the weight is worng \n");
         return 1;
     }
 
-    printf("\n start pour the water in beaker %.0f ml...\n", POUR);
+    printf("\n start pour the water in beaker %.0f ml...\n", b);
 
-    while (water_volume < weight_g && water_volume < BEAKER_CAPACITY) {
-        water_volume += POUR;
+    while (d < c && d < a) {
+        d += b;
         pours++;
-        printf("Pour  %d: Volume of water = %.0f ml\n", pours, water_volume);
+        printf("Pour  %d: Volume of water = %.0f ml\n", pours, d);
     }
 
-    if (water_volume >= weight_g && water_volume <= BEAKER_CAPACITY) {
+    if (d >= c && d <= a) {
         printf("\n apple start floating afer pour of water %d time (Volume of water %.0f ml)\n",
-               pours, water_volume);
+               pours, d);
     } else {
         printf("\n Water is full of %.0f ml this apple can't floating \n",
-               BEAKER_CAPACITY);
+               a);
     }
 
     return 0;
 }
+
+//apple must only have the weight >= volum of water to make the aplple floating 
+//
